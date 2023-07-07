d893a32975639713aa95beb22a558e129bcf4892
eduardomrr03 <eduardomrr03@gmail.com>
2023-07-07 18:21:28 -0500

    Creación de rutas de los componentes

--- LibreriaSalvadorProject/src/app/app-routing.module.ts
+++ LibreriaSalvadorProject/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
