0e94d665afdd492b3d3683e7bb4a0422f7531bbd
eduardomrr03 <eduardomrr03@gmail.com>
2023-07-07 18:10:56 -0500

    Creacion de Componentes para la Aplicacion

--- LibreriaSalvadorProject/src/app/app.module.ts
+++ LibreriaSalvadorProject/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
